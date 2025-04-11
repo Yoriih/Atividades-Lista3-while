ec3ba10f9a17b5d488e0df6909472d3565ec0ebe
Yori <61568561+Yoriih@users.noreply.github.com>
2025-04-10 21:19:37 -0300

    Add files via upload

---
+++ Exercício1.py
@@ -0,0 +1,15 @@
+# Exercício 1: Contagem Crescente
+
+# Crie um loop que conte de 1 a 10 
+
+contato = 1
+
+# Mostre cada número.
+# While significa enquanto, ou seja, enquanto a condição for verdadeira o loop continua.
+# O loop só para quando a condição for falsa.
+# No caso, enquanto contato for menor ou igual a 10, o loop continua.
+
+while contato <= 10:
+    print(contato)
+    contato += 1
+    print("Contando...")
---
+++ Exercício2.py
@@ -0,0 +1,11 @@
+# Exercício 2: Contagem Regressiva
+
+# Crie um loop que conte de 10 a 1
+
+contator = 10 # Inicializa o contador em 10
+
+# Mostre cada número.
+
+while contator > 0: # Enquanto o contador for maior que 0
+    print(contator) # Mostra o número atual
+    contator -= 1 # Decrementa o contador em 1
