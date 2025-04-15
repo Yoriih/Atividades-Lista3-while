19a838f9db44ff00da6d55e7b63a567d13be518e
Yori <61568561+Yoriih@users.noreply.github.com>
2025-04-14 21:02:39 -0300

    Update Exercício1.py

--- Exercício1.py
+++ Exercício1.py
@@ -2,14 +2,13 @@
 
 # Crie um loop que conte de 1 a 10 
 
-contato = 1
+contador = 1
 
 # Mostre cada número.
 # While significa enquanto, ou seja, enquanto a condição for verdadeira o loop continua.
 # O loop só para quando a condição for falsa.
 # No caso, enquanto contato for menor ou igual a 10, o loop continua.
 
-while contato <= 10:
-    print(contato)
-    contato += 1
-    print("Contando...")
+while contador <= 10:
+    print(contador)
+    contador = contador + 1
