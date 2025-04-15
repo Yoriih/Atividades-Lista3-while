5bb57f8d6b1c4fef8587fe03d176c8c0348a688f
Yori <61568561+Yoriih@users.noreply.github.com>
2025-04-14 21:03:10 -0300

    Update Exercício2.py

--- Exercício2.py
+++ Exercício2.py
@@ -2,10 +2,10 @@
 
 # Crie um loop que conte de 10 a 1
 
-contator = 10 # Inicializa o contador em 10
+contador = 10                                   # Inicializa o contador em 10
 
 # Mostre cada número.
 
-while contator > 0: # Enquanto o contador for maior que 0
-    print(contator) # Mostra o número atual
-    contator -= 1 # Decrementa o contador em 1
+while contador > 0:                             # Enquanto o contador for maior que 0
+    print(contador)                             # Mostra o número atual
+    contador = contador - 1                     # Decrementa o contador em 1
