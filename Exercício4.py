b67b969d9f78f7b25cfaf6c1d696aa9a85898216
Yori <61568561+Yoriih@users.noreply.github.com>
2025-04-14 21:03:51 -0300

    Update Exercício4.py

--- Exercício4.py
+++ Exercício4.py
@@ -2,11 +2,11 @@
 
 # Peça ao usuário para digitar um número 
 contador = 1
-numero = int(input("Digite um número: ")) # Solicita ao usuário um número inteiro
+numero = int(input("Digite um número: "))           # Solicita ao usuário um número inteiro
 
 # Mostre a tabuada desse número (de 1 a 10).
 
-while contador <= 10: # Enquanto o contador for menor ou igual a 10
-    resultado = numero * contador # Calcula o resultado da multiplicação
-    print(f"{numero} x {contador} = {resultado}") # Imprime a tabuada
-    contador += 1 # Incrementa o contador em 1
+while contador <= 10:                               # Enquanto o contador for menor ou igual a 10
+    resultado = numero * contador                   # Calcula o resultado da multiplicação
+    print(f"{numero} x {contador} = {resultado}")   # Imprime a tabuada
+    contador = contador + 1                         # Incrementa o contador em 1
