b85cca5a7971b03f77d15fa87cef5894735804fe
Yori <61568561+Yoriih@users.noreply.github.com>
2025-04-14 21:15:50 -0300

    Add files via upload

---
+++ Exercício10.py
@@ -0,0 +1,7 @@
+# Exercício 10: Média de Notas com Condição de Saída
+
+# Peça ao usuário para digitar notas até que ele digite um valor negativo. 
+
+
+
+# Calcule e mostre a média das notas digitadas.
